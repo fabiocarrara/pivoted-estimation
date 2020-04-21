203925e89d55c3e04c96105583bab7bad2ccff99
Fabio Carrara <fabio.carrara@isti.cnr.it>
2020-04-21 13:36:33 +0200

    separated model definition

---
+++ model.py
@@ -0,0 +1,70 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def get_model(params):
+
+    if params.model == 'mlp':
+        return MLP(params)
+        
+    elif params.model == 'res-mlp':
+        return ResMLP(params)
+        
+
+class MLP(nn.Module):
+
+    def __init__(self, params):
+        super(MLP, self).__init__()
+    
+        stage_fn = self._bn_stage if params.batch_norm else self._linear_stage
+        self.stages = nn.ModuleList([stage_fn(params.dim) for _ in range(params.depth)])
+        self.dropout = nn.Dropout(params.dropout)
+        self.last = nn.Linear(params.dim, params.dim)
+    
+    def forward(self, x):
+        for stage in self.stages:
+            x = stage(x)
+            x = F.relu(x)
+            
+        x = self.dropout(x)
+        x = self.last(x)
+        return x
+    
+    @staticmethod
+    def _linear_stage(dim):
+        return nn.Linear(dim, dim)
+    
+    @staticmethod
+    def _bn_stage(dim):
+        return nn.Sequential(
+            nn.Linear(dim, dim),
+            nn.BatchNorm1d(dim)
+        )
+        
+
+class ResMLP(nn.Module):
+
+    def __init__(self, params):
+        super(MLP, self).__init__()
+    
+        self.blocks = nn.ModuleList([self._res_block(params.dim) for _ in range(params.depth)])    
+        self.dropout = nn.Dropout(params.dropout)
+        self.last = nn.Linear(params.dim, params.dim)    
+    
+    def forward(self, x):
+        for block in self.blocks:
+            x += block(x)
+        x = self.dropout(x)
+        x = self.last(x)
+        return x
+
+    @staticmethod
+    def _res_block(dim):
+        return nn.Sequential(
+            nn.Linear(dim, dim),
+            nn.BatchNorm1d(dim),
+            nn.ReLU(),
+            nn.Linear(dim, dim),
+            nn.BatchNorm1d(dim)
+        )
+
